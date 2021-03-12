bac25c708e8e7ff52176f969d6d6d256d88cc729
Shelden <shelden@berkeley.edu>
2021-03-12 11:09:03 -0800

    added deployment of sale

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,5 +1,13 @@
 const BPTToken = artifacts.require('BPTToken');
+const BPTTokenSale = artifacts.require("BPTTokenSale");
 
 module.exports = async function (deployer) {
   await deployer.deploy(BPTToken,'BPTToken', 'BPT', '1000000000');
+  const token = await BPTToken.deployed();
+
+  const testAccount = "0x4f6B17F9ab25070505535402fa48038Cb92F6c91"
+  await deployer.deploy(BPTTokenSale, 1, testAccount, token.address);
+
+  const crowdsale = await BPTTokenSale.deployed();
+  token.tansfer(crowdsale.address, await token.totalSupply())
 };
